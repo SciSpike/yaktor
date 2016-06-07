d290ba8c80c58787f79da4b5cd237010f2a927bc
Matthew T. Adams <matthew@matthewadams.me>
2016-06-07 14:01:08 -0500

    fix dns for mac:  "network" -> "default"

--- bin/static/ROOT/osx-docker-machine-route.sh
+++ bin/static/ROOT/osx-docker-machine-route.sh
@@ -3,4 +3,4 @@
 eval $(docker-machine env default)
 DOCKER_IP=$(docker-machine ip default)
 
-sudo route -n add $(docker network inspect --format '{{ range .IPAM.Config }}{{ .Subnet}}{{end}}' ${PWD##*/}_network) $DOCKER_IP
+sudo route -n add $(docker network inspect --format '{{ range .IPAM.Config }}{{ .Subnet}}{{end}}' ${PWD##*/}_default) $DOCKER_IP
