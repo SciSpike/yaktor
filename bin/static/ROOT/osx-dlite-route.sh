44f8a8a72078e88fe1cafe6951f051ac1c4f4811
Matthew T. Adams <matthew@matthewadams.me>
2016-06-07 14:06:20 -0500

    update osx-dlite-route.sh

--- bin/static/ROOT/osx-dlite-route.sh
+++ bin/static/ROOT/osx-dlite-route.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 DOCKER_IP=$(docker network inspect --format '{{ index .Options "com.docker.network.bridge.host_binding_ipv4" }}' bridge)
-sudo route -n add $(docker network inspect --format '{{ range .IPAM.Config }}{{ .Subnet}}{{end}}' ${PWD##*/}_network) $DOCKER_IP
+sudo route -n add $(docker network inspect --format '{{ range .IPAM.Config }}{{ .Subnet}}{{end}}' ${PWD##*/}_default) $DOCKER_IP
 INTERFACE=$(route get $DOCKER_IP | grep interface: | cut -f 2 -d:); sudo ifconfig ${INTERFACE} -hostfilter $(ifconfig ${INTERFACE} | grep member: | cut -f 2 -d: | cut -f 2 -d\ )
