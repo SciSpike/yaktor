deb45e36f2ffd0286b2ae9a32c278de21d7a80f2
JK <jonathan.kamke@gmail.com>
2016-06-16 16:05:50 -0700

    fix broken UID

--- bin/static/docker/.setup.sh
+++ bin/static/docker/.setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Ensures current user is the owner of /app's files
-UID=$(id -u)
+APP_UID=$(id -u)
 FILE_OWNER_UID=$(stat -c '%u' /app/package.json)
 USER=$(id -un)
-if [ ! $UID == $FILE_OWNER_UID ]; then
-  echo "Changing ${USER}'s uid from $UID to $FILE_OWNER_UID"
+if [ ! $APP_UID == $FILE_OWNER_UID ]; then
+  echo "Changing ${USER}'s uid from $APP_UID to $FILE_OWNER_UID"
   sudo usermod -u $FILE_OWNER_UID $USER
 fi
 
