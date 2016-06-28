260333a268cded7a1600a1da600563412f7919a5
JK <jonathan.kamke@gmail.com>
2016-06-28 10:08:47 -0700

    remove unused

--- osx-docker-machine-route.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-eval $(docker-machine env default)
-DOCKER_IP=$(docker-machine ip default)
-
-sudo route -n add $(docker network inspect --format '{{ range .IPAM.Config }}{{ .Subnet}}{{end}}' bridge) $DOCKER_IP
-sudo route -n add $(docker network inspect --format '{{ range .IPAM.Config }}{{ .Subnet}}{{end}}' ${PWD##*/}_default) $DOCKER_IP
-
-#INTERFACE=$(route get $DOCKER_IP | grep interface: | cut -f 2 -d: | xargs); echo sudo ifconfig ${INTERFACE} -hostfilter $(ifconfig ${INTERFACE} | grep member: | cut -f 2 -d: | cut -f 2 -d\ )
