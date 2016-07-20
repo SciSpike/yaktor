236d835999ba8f27efce266b20ef13fac251fec2
JK <jonathan.kamke@gmail.com>
2016-07-20 13:06:59 -0700

    align image version

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it -v ~/.npmrc:"/home/$(id -un)/.npmrc" -v "$PWD":/app --rm yaktor/yaktor:0.2 $@
+docker run -it -v ~/.npmrc:"/home/$(id -un)/.npmrc" -v "$PWD":/app --rm yaktor/yaktor:0.32.0 $@
