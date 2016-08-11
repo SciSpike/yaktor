55cd7971c8c32534077ba3df6157630bc5446681
JK <jonathan.kamke@gmail.com>
2016-08-11 09:56:28 -0700

    use yaktor/node:0.39.0

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # should use the latest available image to validate, but not LATEST
-docker run -it --rm --user node -v "$PWD":/app yaktor/node:0.38.0 $@
+docker run -it --rm --user node -v "$PWD":/app yaktor/node:0.39.0 $@
