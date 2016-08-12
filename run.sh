75bec7adff0302fba5f3a8155727cc20a0dd68f4
JK <jonathan.kamke@gmail.com>
2016-08-12 15:15:47 -0700

    try to get branch into coveralls

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # should use the latest available image to validate, but not LATEST
-docker run -it --rm --user node -v "$PWD":/app yaktor/node:0.39.0 $@
+if [ -f .env ]; then
+RUN_ENV_FILE='--env-file .env'
+fi
+docker run $RUN_ENV_FILE -it --rm --user node -v "$PWD":/app yaktor/node:0.39.0 $@
